44a94267b30a8b4de17da72ec827391a26689ba3
ZETA PRAWIRA SYAH <zeta.prawira@ui.ac.id>
2023-03-07 21:06:53 +0700

    PBI 1 Register

---
+++ context/AuthenticationContext.js
@@ -0,0 +1,102 @@
+import { createContext, useState } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+const AuthenticationContext = createContext()
+
+export const AuthenticationProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+	const [accessToken, setAccessToken] = useState(null)
+	const [error, setError] = useState(null)
+
+	const router = useRouter()
+
+    // Login
+	const login = async ({email, password}) => {
+		const config = {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		}
+
+		const body = {
+			email,
+			password
+		}
+
+		try {
+			const { data:access } = await axios.post('http://localhost:3001/api/login', body, config)
+			if(access && access.user){
+				setUser(access.user)
+			}
+
+			if(access && access.token){
+				setAccessToken(access.token)
+			}
+
+			router.push('/')
+
+		} catch(error){
+			if (error.response & error.response.data) {
+				setError(error.response.data.message)
+				return      
+			} else if (error.request) {
+			  setError('Something went wrong')
+			  return  
+			} else {
+			  setError('Something went wrong')
+			  return
+			}
+			console.error('Error', error.message);
+			setError('Something went wrong')
+			return
+		}
+
+		
+	}
+    // Register
+    const register = async ({name, email, password, phone}) => {
+        const config = {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		}
+
+        const body = {
+			name,
+			email,
+			password,
+			phone
+		}
+
+        try {
+			// call nextjs api function to create a user
+			await axios.post('http://localhost:3001/api/register', body, config)
+			login({ email, password })
+		} catch(error) {
+		  if (error.response & error.response.data) {
+		  	setError(error.response.data.message)
+		  	return      
+	      } else if (error.request) {
+		    setError('Something went wrong')
+		    return  
+	      } else {
+			setError('Something went wrong')
+			return
+	      }
+	      console.error('Error', error.message);
+	      setError('Something went wrong')
+	      return
+		}
+    }
+
+	return (
+		<AuthenticationContext.Provider value={{ user, accessToken, error, login, register}}>
+			{children}
+		</AuthenticationContext.Provider>
+	)
+}
+
+export default AuthenticationContext;
---
+++ pages/api/login.js
@@ -0,0 +1,62 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+import cookie from "cookie";
+
+export default async (req, res) => {
+    let accessToken = null;
+
+    if (req.method === 'POST') {
+        const {email, password} = req.body
+        const config = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+    
+        const body = {
+            email,
+            password
+        }
+
+        try{
+            const {data} = await axios.post('http://localhost:8000/authuser/login', body, config) 
+            accessToken = data.token
+        } catch(error){
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.error(error.response.data);
+              console.error(error.response.status);
+              console.error(error.response.headers);
+              return res.status(401).json({message: error.response.data.detail})
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.error(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.error('Error', error.message);
+            }
+            console.error(error.config);
+      
+            return res.status(500).json({message: 'Something went wrong'})
+          }
+        
+
+        if(accessToken){
+            const userConfig ={
+                headers: {
+                  'Authorization': `Bearer ${accessToken}`
+                }
+            }
+            const {data: user} = await axios.get('http://localhost:8000/authuser/get-user-session', userConfig)
+            res.status(200).json({user, accessToken})
+        }
+        
+    }else{
+        res.setHeader('Allow', ['POST'])
+        res.status(405).json({message: `Method ${req.method} is not allowed`})
+    }
+}
